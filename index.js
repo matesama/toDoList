024c1b730dee052ac18f1924542cd179ec64d2f2
Mateo Santa <mateo.santamanrique@gmail.com>
2023-07-08 15:41:51 +0200

    edit,done,delete function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const input = document.querySelector('input');
+//const input = document.querySelector('input');
 const form = document.querySelector("form");
 const list = document.querySelector("#listItems");
+const itemsInput = form.elements['items'];
+const inputText = document.querySelector('#txt');
 
 const submitFunction = (event) => {
     event.preventDefault();
@@ -9,12 +11,38 @@ const submitFunction = (event) => {
     if (itemsInput.value.trim() === "") {
         alert("There is not input. Please enter a value before submitting");
     } else {
-        console.log(itemsInput.value);
+            //item
            let newItem = document.createElement('li');
+
            let newItemContent = document.createTextNode(`${itemsInput.value}`);
            newItem.appendChild(newItemContent);
+           newItem.setAttribute('class', `item`);
            list.appendChild(newItem);
-          
+            //edit
+            let editButton = document.createElement("BUTTON");
+            let editText = document.createTextNode('Edit');
+            editButton.appendChild(editText);
+            editButton.setAttribute("class", `edit-button` );
+            newItem.appendChild(editButton); 
+            //done
+            let doneButton = document.createElement("BUTTON");
+            let doneText = document.createTextNode('Done');
+            doneButton.appendChild(doneText);
+            doneButton.setAttribute("class", `done-button`);
+            newItem.appendChild(doneButton); 
+            //tick
+            let tickButton = document.createElement("INPUT");
+            tickButton.setAttribute("type", "checkbox")
+            tickButton.setAttribute("class", `tick-button`);
+            newItem.appendChild(tickButton);
+            //delete
+            let deleteButton = document.createElement("BUTTON");
+            let buttonText = document.createTextNode('Delete');
+            deleteButton.appendChild(buttonText);
+            deleteButton.setAttribute("class", `delete-button`);
+            newItem.appendChild(deleteButton); 
+
+            
           
            //window.localStorage.setItem('item', `${itemsInput.value}`);
            //document.getElementById("test").innerHTML = window.localStorage.getItem("item");
@@ -23,17 +51,20 @@ const submitFunction = (event) => {
     }
 }
 
-const editButton = (event) => {
+
+
+/*const editButton = (event) => {
     event.preventDefault();
     const itemsInput = form.elements['items'];
 
     if (itemsInput.value.trim() === "") {
         console.log("There is not input. Please enter a value before submitting");
     } else {
-    let edButton = document.createElement("BUTTON");
+    let editButton = document.createElement("BUTTON");
     let buttonText = document.createTextNode('Edit');
-    edButton.appendChild(buttonText);
-    list.appendChild(edButton); 
+    editButton.appendChild(buttonText);
+    editButton.setAttribute("class", `edit-button` );
+    list.appendChild(editButton); 
     }
 
 }
@@ -44,10 +75,11 @@ const doneButton = (event) => {
     if (itemsInput.value.trim() === "") {
         console.log("There is not input. Please enter a value before submitting");
     } else {
-    let edButton = document.createElement("BUTTON");
+    let doneButton = document.createElement("BUTTON");
     let buttonText = document.createTextNode('Done');
-    edButton.appendChild(buttonText);
-    list.appendChild(edButton); 
+    doneButton.appendChild(buttonText);
+    doneButton.setAttribute("class", `done-button`);
+    list.appendChild(doneButton); 
     }
 }
 
@@ -60,12 +92,81 @@ const tickButton = (event) => {
     } else {
     let tickButton = document.createElement("INPUT");
     tickButton.setAttribute("type", "checkbox")
+    tickButton.setAttribute("class", `tick-button`);
     list.appendChild(tickButton); 
     }
 
 }
 
+const deleteButton = (event) => {
+    event.preventDefault();
+    const itemsInput = form.elements['items'];
+
+    if (itemsInput.value.trim() === "") {
+        console.log("There is not input. Please enter a value before submitting");
+    } else {
+    let deleteButton = document.createElement("BUTTON");
+    let buttonText = document.createTextNode('Delete');
+    deleteButton.appendChild(buttonText);
+    deleteButton.setAttribute("class", `delete-button`);
+    list.appendChild(deleteButton); 
+    }
+}
+*/
 form.addEventListener('submit', submitFunction);
-form.addEventListener('submit', editButton);
+/*form.addEventListener('submit', editButton);
 form.addEventListener('submit', doneButton);
 form.addEventListener('submit', tickButton);
+form.addEventListener('submit', deleteButton);
+*/
+//Query selector id listIItems
+
+
+//Conditional to check if you are clicking editButton => event.target.classList.contains("edit-button")
+const editFunction = (event) => {
+    console.log('event', event)
+    event.preventDefault();
+    
+
+    if(event.target.classList.contains("edit-button")) {
+        //event.target.parentNode.querySelector("li");
+        event.target.parentNode.contentEditable = true;
+       event.target.parentNode.style.backgroundColor = "#dddbdb";
+    } else if(event.target.classList.contains("done-button")){
+        event.target.parentNode.contentEditable = false;
+        event.target.parentNode.style.backgroundColor = "#ffffff";
+
+    } else if(event.target.classList.contains("delete-button")) {
+        event.target.parentNode.remove();
+        
+        
+        
+
+    } else if(event.target.classList.contains("tick-button")){
+
+    }
+
+};
+//event.target.parentNode.querySelector(".container")
+//Add event listener click (event)
+
+listItems.addEventListener('click', editFunction);
+
+
+
+
+
+
+
+/*list.addEventListener('click', (event) => {
+    if(event.target.tagName === 'BUTTON') {
+        const button = event.target;
+        const li = button.parentNode;
+        const ul = li.parentNode;
+        if(button.textContent === 'Done') {
+            list.removeChild(li);
+        }
+
+
+    }
+})*/
