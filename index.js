3dda96a0b771b221e11f6a271bd07d0fc1ed9cc6
Mateo Santa <mateo.santamanrique@gmail.com>
2023-07-08 17:59:39 +0200

    first OOP Version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//const input = document.querySelector('input');
+/* //const input = document.querySelector('input');
 const form = document.querySelector("form");
 const list = document.querySelector("#listItems");
 const itemsInput = form.elements['items'];
@@ -47,6 +47,7 @@ const submitFunction = (event) => {
 }
 
 form.addEventListener('submit', submitFunction);
+
 //Conditional to check if you are clicking editButton => event.target.classList.contains("edit-button")
 const editFunction = (event) => {
     console.log('event', event)
@@ -70,3 +71,86 @@ const editFunction = (event) => {
 };
 
 listItems.addEventListener('click', editFunction);
+*/
+
+// OOP Version:
+           let newItem = document.createElement('li');
+           let newItemText = document.createElement('div');
+
+           let newItemContent = document.createTextNode(`${itemsInput.value}`);
+           newItemText.appendChild(newItemContent);
+           newItem.appendChild(newItemText);
+           newItem.setAttribute('class', `item`);
+           list.appendChild(newItem);
+            //edit
+            let editButton = document.createElement("BUTTON");
+            let editText = document.createTextNode('Edit');
+            editButton.appendChild(editText);
+            editButton.setAttribute("class", `edit-button` );
+            newItem.appendChild(editButton); 
+            //done
+            let doneButton = document.createElement("BUTTON");
+            let doneText = document.createTextNode('Done');
+            doneButton.appendChild(doneText);
+            doneButton.setAttribute("class", `done-button`);
+            newItem.appendChild(doneButton); 
+            //tick
+            let tickButton = document.createElement("INPUT");
+            tickButton.setAttribute("type", "checkbox")
+            tickButton.setAttribute("class", `tick-button`);
+            newItem.appendChild(tickButton);
+            //delete
+            let deleteButton = document.createElement("BUTTON");
+            let buttonText = document.createTextNode('Delete');
+            deleteButton.appendChild(buttonText);
+            deleteButton.setAttribute("class", `delete-button`);
+            newItem.appendChild(deleteButton); 
+
+
+class ListElement{
+    constructor(text){
+        this.text = text;
+        
+        }
+
+    get text(){
+        return this.text;
+    }
+    
+    set text(newText){
+        this.text = newText;
+
+    }
+}
+
+class TODOList {
+    #items
+   constructor() {
+    this.#items = [];
+    this.form = document.querySelector("form");
+    this.list = document.querySelector("#listItems");
+    this.form.addEventListener('submit', submitFunction);
+   }
+
+    get items(){
+          return this.#items;
+        }
+        
+    addItem(placeholder){
+        
+        this.text = document.createTextNode(`${itemsInput.value}`);
+        this.#items.push(placeholder);
+    }
+    
+
+}
+
+
+const myToDoList = new TODOList();
+const item1 = new ListElement();
+console.log(item1);
+
+
+myBookList.addItem(new ListElement);
+
+
